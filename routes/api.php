cd21385166c41918d06a0eef2d86b8a7cfe99e68
Jesse Songok <jessesongok@gmail.com>
2024-08-12 09:58:41 +0300

    Feature-> API Logout

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 
-Route::get("/user", function (Request $request) {
-    return $request->user();
-})->middleware("auth:sanctum");
-
 Route::post("/login", function (Request $request) {
     $request->validate([
         "email" => ["required", "email"],
@@ -19,13 +15,24 @@ Route::post("/login", function (Request $request) {
 
     $user = User::where("email", $request->email)->first();
 
-    if (! $user || ! Hash::check($request->password, $user->password)) {
+    if (!$user || !Hash::check($request->password, $user->password)) {
         throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
+            "email" => ["The provided credentials are incorrect."],
         ]);
     }
 
     return response()->json([
         "token" => $user->createToken($request->device_name)->plainTextToken,
     ]);
+});
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::post("/logout", function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    });
+
+    Route::get("/user", function (Request $request) {
+        return $request->user();
+    });
 });
